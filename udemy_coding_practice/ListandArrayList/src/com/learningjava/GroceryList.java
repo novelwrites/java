d5c84a38c55de1daf140a91a23b0ba9c2751c407
Nancy Golden <pngolden@verizon.net>
2020-06-22 23:15:47 -0500

    more coding from watching Udemy videos

---
+++ udemy_coding_practice/ListandArrayList/src/com/learningjava/GroceryList.java
@@ -0,0 +1,39 @@
+package com.learningjava;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item); //how you add elements to the array list
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+
+        for (int i=0; i<groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+
+        groceryList.set(position, newItem);
+
+        System.out.println("Grocery item " + (position+1) + "has been modified");
+
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        //Milk
+        //Cheese //Will automatically shift array elements when an item is removed
+        //Bread
+
+
+    }
+}
---
+++ udemy_coding_practice/ListandArrayList/src/com/learningjava/Main.java
@@ -0,0 +1,10 @@
+package com.learningjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Arraylist - a resizable array - resizes automatically
+
+
+    }
+}
