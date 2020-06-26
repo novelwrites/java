776e15d3227c53fd76af9d7fcde1066c8e0b53d6
Nancy Golden <pngolden@verizon.net>
2020-06-26 18:40:27 -0500

    really starting to understand the basics

--- udemy_coding_practice/ReviewAndPractice/src/Hello.java
+++ udemy_coding_practice/ReviewAndPractice/src/Hello.java
@@ -23,6 +23,12 @@ public class Hello { //body of the class Hello
         operatorPractice();
         ifThenPractice();
         testKnowledge();
+        kilometerConversion ();
+        createCodePractice ();
+        loveMyPhil();
+        calculateScore(myFirstNumber);
+        tryIfThenElse();
+
 
 
     }
@@ -94,6 +100,109 @@ public class Hello { //body of the class Hello
             System.out.println("I wish I could horsebackride today.");
             System.out.println("newnum * newNum2 = " + multiNum);
         }
+
+        int topScore = 100;
+        if(topScore<=100) { //does this evaluate to true? If so, execute code block
+            System.out.println("You got the top score!");
+        }
+
+        int topScore2 = 50;
+        if(topScore2 == 49 || topScore == 100) {
+            System.out.println("Top score1 is higher than top score2");
+        } else System.out.println("Top score2 is lower than top score2");
+
+        int bayHorses = 15;
+        int sorrelHorses = 12;
+        int paintHorses = 25;
+
+        if (paintHorses < bayHorses && sorrelHorses < bayHorses) {
+            System.out.println("We need more horses!!!");
+            System.out.println("Let's buy some buckskins!");
+        } else {
+            System.out.println("We have enough horses (which should never be true).");
+
+        }
+
+        double  appleSlices = 35;
+        double orangeSlices = 23;
+        if(appleSlices > 30 || orangeSlices == 10) {
+
+            System.out.println("Yay! We have enough fruit!");
+        }
+
+        boolean haveCar = false;
+        if (!haveCar) {
+
+            System.out.println("We don't have a car!");
+        }
+
+        boolean isCar = true;
+        if (isCar) {
+            System.out.println("Prints if isCar equals true");
+        }
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true because isCar is true");
+        }
+
+    }
+    public static void kilometerConversion () {
+        double miles = 25;
+        double kilometers = miles * 1.609344;
+        System.out.println("25 miles = " + kilometers);
+    }
+
+    public static void createCodePractice () {
+
+       boolean ifGameOver = true;
+       int score = 250;
+       int levelCompleted = 5;
+       int bonus = 500;
+
+       if (ifGameOver && score > 500) {
+           System.out.println("Game is over and the score was over 500.");
+       } else {
+           System.out.println("Game might not be over and score might be less than 500");
+       }
+
+    }
+
+    public static void loveMyPhil() {
+
+        String handsomeHusband = "Phil";
+        String beautifulBride = "Nancy";
+        Boolean madlyInLove = true;
+
+        if (handsomeHusband == "Phil" && beautifulBride == "Nancy" && madlyInLove) {
+            System.out.println("We are so blessed - thank you to our Lord Jesus Christ!");
+        }
+        else {
+            System.out.println("It's the wrong name.");
+        }
+    }
+
+    public static void tryIfThenElse() {
+
+        int try1 = 50;
+
+        if (try1 == 30) {
+            System.out.println("try1 = 30");
+        }
+            else if (try1 == 40) {
+                System.out.println("try1 = 40");
+            }
+            else {
+            System.out.println("We don't know what try1 is equal to.");
+
+        }
+
+    }
+
+    public static void calculateScore(int myFirstNumber) {
+        if(myFirstNumber > 1) {
+            System.out.println("myFirstNumber is greater than 1");
+        }
+
     }
 }
 
