5a833e11a47ea46f680a06b9c277e02eceee8899
Nancy Golden <pngolden@verizon.net>
2020-06-28 20:51:46 -0500

    Working on undertanding classes and getters and setters

---
+++ udemy_coding_practice/Classes/src/com/learningjava/Horse.java
@@ -0,0 +1,23 @@
+package com.learningjava;
+
+public class Horse {
+
+    private String discipline;
+    private String color;
+    private String name;
+    private int age;
+
+    public void setColor(String color) {
+        String validColor = color.toLowerCase();
+        if (validColor.equals("bay") || validColor.equals("sorrel")) {
+            this.color = color;
+        } else {
+            this.color = "Unknown";
+        }
+    }
+        public String getColor() { //This is a method that can be called elsewhere to return the current color.
+            return this.color;
+
+
+        }
+    }
