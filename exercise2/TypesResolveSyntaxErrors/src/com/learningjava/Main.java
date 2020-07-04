b75574be3fa3476e238059846050e6f484311380
Nancy Golden <pngolden@verizon.net>
2020-07-03 23:38:45 -0500

    I feel good about this one because I successfully corrected the syntax errors rather quickly and it was both fun and good practice.

---
+++ exercise2/TypesResolveSyntaxErrors/src/com/learningjava/Main.java
@@ -0,0 +1,45 @@
+package com.learningjava;
+
+import java.util.Arrays;
+
+public class Main {
+
+
+
+
+
+
+    public static void main(String[] args) {
+//    This class has syntax errors, that will prevent your other classes from running.
+//    So... work on this file first to resolve the issues.
+      boolean value = true;
+      int [] myArray = {3,4,5,6,76,12,2};
+      String characterMovieA = "Harry and the Hendersons";
+      String characterMovieB = "Star Wars";
+      String male = "m";
+      String female = "f";
+//
+//    //the above instance variables have mistakes with their data types.
+//    Fix the data types so the main method correctly displays the value of all of the variables
+//
+
+//
+    System.out.println("There are variables that have been declared in this Accessible class.  " +
+            "Fix the code so that all the variables can be accessed by the main method.  print out " +
+            "the values of every variable once the code has been refactored.");
+        System.out.println(true + " " + Arrays.toString(myArray) + " " + characterMovieA + " " + characterMovieB + " " + male + " " + female);
+//    }
+    }
+
+
+
+
+
+
+
+
+
+
+	// write your code here
+    }
+
