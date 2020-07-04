775ec24d132c8843110b88f199415fdcab5f999c
Nancy Golden <pngolden@verizon.net>
2020-07-03 22:29:26 -0500

    working on more Module 6 exercises

---
+++ exercise2/Colors/src/com/learningjava/Colors.java
@@ -0,0 +1,28 @@
+
+package com.learningjava;
+
+public class Colors {
+    public void colorful(String[] strings) {
+    }
+}
+
+    //String[] colorful(String[] strings) {
+
+        //String[] colorArray = {"blue", "red", "yellow", "black", "green", "pink"};
+        //String nameFromArray;
+        //int index;
+
+       // For the given strings, keep all the strings that begin with a color ("red", "yellow",
+       // "green", "blue", etc.) return an array of strings that start with a color.  discard
+       // all the other strings.  If no strings start with a color in the input array, return
+       // an empty array.
+
+       //return new String[]{};
+   //}
+
+  // public String findColor(String )
+
+
+
+
+
